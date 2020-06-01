f1e803e0859a0de6de77a4d1c527253f301c09a3
Harry Nguyen <phuquyit@gmail.com>
2020-06-01 20:37:44 +0700

    [Thực hành] Ứng dụng login

---
+++ src/main/java/service/UserService.java
@@ -0,0 +1,44 @@
+package service;
+
+import model.Login;
+import model.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserService {
+    public static List<User> users;
+    static {
+        users = new ArrayList<>();
+        User u1 = new User();
+        u1.setAccount("tuan");
+        u1.setPassword("123456");
+        u1.setEmail("Tuan@123.com");
+        u1.setName("Tuấn");
+        u1.setAge(18);
+
+        User u2 = new User();
+        u2.setAccount("binh");
+        u2.setPassword("123456");
+        u2.setEmail("Binh@123.com");
+        u2.setName("Bình");
+        u2.setAge(17);
+
+        User u3 = new User();
+        u3.setAccount("duc");
+        u3.setPassword("123456");
+        u3.setEmail("Duc@123.com");
+        u3.setName("Đức");
+        u3.setAge(19);
+    }
+
+    public static User checkLogin (Login login){
+        for (User u: users){
+            if (u.getAccount().equals(login.getAccount())
+                && u.getAccount().equals(login.getPassword())){
+                return u;
+            }
+        }
+        return null;
+    }
+}
